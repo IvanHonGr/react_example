bf328d5368c94b559a8ab7625cfafe3e31648f1d
ivan <i.honcharenko@lerpal.com>
2018-11-05 17:42:21 +0200

    initial

---
+++ src/js/test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+console.log('Hello ninjas!');
+
+export default function Test(props) {
+	return(
+	<div>
+		<h1>Hello ninja, reactive ninja</h1>
+		<p>not is {new Date().toString()}</p>
+	</div>
+	);
+}
---
+++ webpack-config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname + '/src/js',
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+	extensions: ['.js', '.jsx']
+  },
+  devServer: {
+	contentBase: [
+	  __dirname + '/dist',
+	  __dirname + '/src/static']
+  },
+  module: {
+  rules: [
+    {
+	  test: /\.jsx?$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015', 'react']
+        }
+      }
+    }
+   ]
+  }
+  
+};
